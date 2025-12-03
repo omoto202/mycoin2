10d938ba225437f515fa0c15685a1d8fe5f0aea8
omoto202 <kazukiomoto12@gmail.com>
2025-12-03 19:54:43 +0900

    miner.py

--- miner.py
+++ miner.py
@@ -2,11 +2,16 @@ import threading
 import time
 
 class Miner:
-    def __init__(self, blockchain, broadcast_fn=None):
+    def __init__(self, blockchain, broadcast_fn=None, broadcast_done_fn=None):
         self.blockchain = blockchain
         self._running = False
         self.thread = None
-        self.broadcast_fn = broadcast_fn
+        self.broadcast_fn = broadcast_fn            # 新ブロック通知
+        self.broadcast_done_fn = broadcast_done_fn  # マイニング完了通知（オプション）
+
+    def is_mining(self):
+        """現在採掘中かどうか UI 判定用"""
+        return self._running
 
     def start_mining_once(self, miner_address, reward=1):
         # run mining in background thread to avoid blocking request
@@ -20,7 +25,11 @@ class Miner:
         self._running = True
         try:
             block = self.blockchain.mine_block(miner_address, reward)
+            # new block broadcast
             if self.broadcast_fn:
                 self.broadcast_fn(block)
         finally:
             self._running = False
+            # mining done broadcast
+            if self.broadcast_done_fn:
+                self.broadcast_done_fn()
